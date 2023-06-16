524d21fbe47b8aac30b45ceb269c69ba327921ab
VyasG <ngaurav.vyas@gmail.com>
2023-06-16 14:50:14 +1000

    Added logic for calculating player game scores when its not in tiebreaker. Implemented getter,setter and incrementors.

--- src/Player.java
+++ src/Player.java
@@ -17,4 +17,108 @@ public class Player {
         this.name = name;
     }
 
+    //Getter, setter and incrementers
+    //Register games win
+    public void incrementGamesWon()
+    {
+        gamesWon++;
+    }
+
+    //Register set win
+    public void incrementSetsWon()
+    {
+        setsWon++;
+    }
+
+    //register tiebreaker point won
+    public void incrementTieBreakerPoint()
+    {
+        this.tieBreakerPoint++;
+    }
+
+    /**
+     * Sets scoring Point, when needed to adjust based on other player score such as advantage or deuce.
+     * @param gamePoint Point to be set as players game point
+     */
+    public void setGamePoint(Point gamePoint) {
+        this.gamePoint = gamePoint;
+    }
+
+    public Point getGamePoint() {
+        return gamePoint;
+    }
+
+    public int getGamesWon() {
+        return gamesWon;
+    }
+
+    public int getSetsWon() {
+        return setsWon;
+    }
+
+
+    public int getTieBreakerPoint() {
+        return tieBreakerPoint;
+    }
+
+    /**
+     * resetGame  method resets all scoring point vriables when its concluded.
+     */
+    public void resetGame() {
+        this.tieBreakerPoint =0;
+        this.gamePoint = Point.ZERO;
+    }
+
+    /**
+     * getNextPoint method calculates and return next scored point for the player based on other players point scores in a game
+     * @param otherPlayer Other player in the Match
+     * @return Point scored by the player
+     */
+    public Point  getNextPoint( Player otherPlayer)
+    {
+        Point point = Point.ZERO;
+        switch (this.gamePoint)
+        {
+            case ZERO:
+                point = Point.FIFTEEN;
+                break;
+            case FIFTEEN:
+                point = Point.THIRTY;
+                break;
+            case THIRTY:
+                point = Point.FORTY;
+                break;
+            case FORTY:
+            {
+                point =   Point.GAME;
+                if (otherPlayer.getGamePoint() == Point.FORTY) {
+                    point =   Point.ADVANTAGE;
+                } else if (otherPlayer.getGamePoint() == Point.ADVANTAGE) {
+                    otherPlayer.setGamePoint(Point.FORTY);
+                    point =   Point.FORTY;
+                }
+
+            }
+            break;
+
+            case ADVANTAGE:
+                point =  Point.GAME;
+                break;
+            default:
+                point = Point.ZERO;
+                break;
+        }
+
+        //Win prompts a reset
+        if(point == Point.GAME)
+        {
+            point = Point.ZERO;
+            incrementGamesWon();
+            this.resetGame();
+            otherPlayer.resetGame();
+        }
+        return point;
+    }
+
+
 }
